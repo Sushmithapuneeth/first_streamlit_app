32b5ffadc3c4420484f0876b2011edab1dd45056
Sushmitha Puneeth <122335181+Sushmithapuneeth@users.noreply.github.com>
2023-01-10 11:16:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('I love myself more')
 
-streamlit.Breakfast Menu 
+Breakfast Menu 
 ('omega 3 &  Blueberry oatmeal
 Kale, Spinach and rocket smoothie
 Hard-boiled free-range egg')
