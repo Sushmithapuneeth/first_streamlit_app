91012f170f20c132b8b17b6c92e84afb0e9906a4
Sushmitha Puneeth <122335181+Sushmithapuneeth@users.noreply.github.com>
2023-01-10 12:01:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🥗Kale, Spinach and rocket smoothie')
 streamlit.text('🐔Hard-boiled free-range egg')
 streamlit.text('🥑🍞 Avacado toast')
 streamlit.header('🍌🥭 Build your own fruit smoothies')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
