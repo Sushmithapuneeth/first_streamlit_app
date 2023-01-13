7fbd1c07856cefd094ce5508c639328bd6425aed
Sushmitha Puneeth <122335181+Sushmithapuneeth@users.noreply.github.com>
2023-01-13 20:54:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,25 +21,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#create the repetable block (called a function)
-def get_fruityvice_data(fruit_choice):
-    from pandas.io.json import json_normalize
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!') 
-try: 
-    fruit_choice = streamlit.text_input('What fruit would you like information about?') 
-    if not fruit_choice: 
-        streamlit.error("Please select a fruit to get information.") 
-    else: 
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(fruityvice_normalized)
-        
-except URLError as e:
-  streamlit.error()
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
   
 
 streamlit.header("The Fruit Load List Contain:")
