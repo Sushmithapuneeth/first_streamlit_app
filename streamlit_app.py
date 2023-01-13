20ec10dda23fc40ed57c5eb0aff231924cb2076e
Sushmitha Puneeth <122335181+Sushmithapuneeth@users.noreply.github.com>
2023-01-13 14:34:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,13 +37,6 @@ try:
   else:
     back_from_function = get_fruityvice_data(this_fruit_choice)
     streamlit.dataframe(back_from_function)
-   
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 #Allow the end user to add fruit to the list
 def insert_snowflake_row(new_fruit):
